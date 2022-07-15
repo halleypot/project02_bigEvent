862eaf8684f119ec015d573d0a84c2efadedfa7b
halleypot <halleypot@163.com>
2022-07-15 23:31:13 +0800

    finish registion part

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,9 @@
+//拼接完整的url路径
+
+$(function() {
+
+    $.ajaxPrefilter( function(option) {
+        option.url = 'http://www.liulongbin.top:3007' + option.url
+        console.log(option.url);
+    })
+})
---
+++ assets/js/login.js
@@ -0,0 +1,53 @@
+$(function () {
+    // swtich login and register box
+    $('#signUp').on('click', function () {
+        // console.log(111);
+        $('.log-box').hide()
+        $('.reg-box').show()
+    })
+
+    $('#signIn').on('click', function () {
+        $('.reg-box').hide()
+        $('.log-box').show()
+    })
+})
+
+// form validation
+let form = layui.form
+
+form.verify({
+    pass: [
+        /^[\S]{6,12}$/
+        , '密码必须6到12位，且不能出现空格'
+    ],
+    repass: function (value) {
+        let pass = $('.reg-box [name=password]').val()
+        if (value !== pass) {
+            return "two password must be the same"
+        }
+    }
+})
+
+
+// create layer object
+let layer = layui.layer
+
+//submit a form
+$('#form-register').on('submit', function (e) {
+    e.preventDefault()
+
+    let data = $(this).serialize()
+    
+    $.post('/api/reguser',
+        data,
+        function (res) {
+            if(res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            // if registration passed, tip user and swtich to login page
+            layer.msg(res.message)
+            //go to login page
+            $('#signIn').click()
+        })
+
+})
