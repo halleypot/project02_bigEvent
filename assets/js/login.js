c7295be97b56760e2243677ab52a00584558754a
halleypot <halleypot@163.com>
2022-07-16 09:37:09 +0800

    change readon function scope

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,67 +10,69 @@ $(function () {
         $('.reg-box').hide()
         $('.log-box').show()
     })
-})
-
-// form validation
-let form = layui.form
-
-form.verify({
-    pass: [
-        /^[\S]{6,12}$/
-        , '密码必须6到12位，且不能出现空格'
-    ],
-    repass: function (value) {
-        let pass = $('.reg-box [name=password]').val()
-        if (value !== pass) {
-            return "two password must be the same"
-        }
-    }
-})
 
 
-// create layer object
-let layer = layui.layer
+    // form validation
+    let form = layui.form
 
-//submit a form
-$('#form-register').on('submit', function (e) {
-    e.preventDefault()
-
-    let data = $(this).serialize()
-    
-    $.post('/api/reguser',
-        data,
-        function (res) {
-            if(res.status !== 0) {
-                return layer.msg(res.message)
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repass: function (value) {
+            let pass = $('.reg-box [name=password]').val()
+            if (value !== pass) {
+                return "two password must be the same"
             }
-            // if registration passed, tip user and swtich to login page
-            layer.msg(res.message)
-            //go to login page
-            $('#signIn').click()
-        })
-
-})
-
-
-// 登录验证
-$('#form-login').on('submit', function(e) {
-    e.preventDefault()
-
-    let data = $(this).serialize()
-
-    $.post('/api/login',data, function(res) {
-        //if wrong username or password
-        if (res.status !== 0) {
-
-            return layer.msg(rse.message)
         }
-
-        //if log in successfully
-        layer.msg(res.message)
-        //store token in localStorage
-        localStorage.setItem('token', res.token)
-
-        location.href ='/index.html'
     })
+
+
+    // create layer object
+    let layer = layui.layer
+
+    //submit a form
+    $('#form-register').on('submit', function (e) {
+        e.preventDefault()
+
+        let data = $(this).serialize()
+
+        $.post('/api/reguser',
+            data,
+            function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                // if registration passed, tip user and swtich to login page
+                layer.msg(res.message)
+                //go to login page
+                $('#signIn').click()
+            })
+
+    })
+
+
+    // 登录验证
+    $('#form-login').on('submit', function (e) {
+        e.preventDefault()
+
+        let data = $(this).serialize()
+
+        $.post('/api/login', data, function (res) {
+            //if wrong username or password
+            if (res.status !== 0) {
+
+                return layer.msg(rse.message)
+            }
+
+            //if log in successfully
+            layer.msg(res.message)
+            //store token in localStorage
+            localStorage.setItem('token', res.token)
+
+            location.href = '/index.html'
+        })
+    })
+
 })
